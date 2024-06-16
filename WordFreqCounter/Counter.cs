1e31c68a039fd331103a9df886d4db208e216918
Garth天卜 <g-art-h@outlook.com>
2024-06-17 05:17:30 +0800

    添加项目文件。

---
+++ WordFreqCounter/Counter.cs
@@ -0,0 +1,139 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace WordFreqCounter
+{
+    internal class Counter
+    {
+        private readonly string _readPath;
+        private readonly string _writePath;
+        private readonly HashSet<int> _extraChars;
+        private readonly Dictionary<string, int> _freqDict;
+        private readonly Dictionary<string, int> _resultDict;
+        private readonly int _wordLength;
+        private readonly int _windowSize;
+        private readonly int _filter;
+
+        public Counter(string readPath, string writePath, HashSet<int> extraChars, int wordLength, int filter)
+        {
+            _readPath = readPath;
+            _writePath = writePath;
+            _extraChars = extraChars;
+            _freqDict = new Dictionary<string, int>(131072);
+            _resultDict = new Dictionary<string, int>(65536);
+            _wordLength = wordLength;
+            _windowSize = wordLength * 2 - 1;
+            _filter = filter;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveOptimization)]
+        private void LoadAllWords()
+        {
+            using StreamReader sr = new(_readPath, Encoding.UTF8);
+            StringBuilder sb = new(_wordLength);
+            while (!sr.EndOfStream)
+            {
+                var span = sr.ReadLine().AsSpan();
+                for (int i = 0; i < span.Length; i++)
+                {
+                    var c = span[i];
+                    if ((c < '一' || c > '鿿') && !_extraChars.Contains(c))
+                    {
+                        sb.Clear();
+                        continue;
+                    }
+                    sb.Append(c);
+                    if (sb.Length == _wordLength)
+                    {
+                        var word = sb.ToString();
+                        ref var freq = ref CollectionsMarshal.GetValueRefOrNullRef(_freqDict, word);
+                        if (Unsafe.IsNullRef(ref freq)) _freqDict.Add(word, 1);
+                        else freq++;
+                        sb.Remove(0, 1);
+                    }
+                }
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveOptimization)]
+        private void LoadGoodWords()
+        {
+            using StreamReader sr = new(_readPath, Encoding.UTF8);
+            StringBuilder sb = new(_windowSize);
+            while (!sr.EndOfStream)
+            {
+                var span = sr.ReadLine().AsSpan();
+                for (int i = 0; i < span.Length; i++)
+                {
+                    var c = span[i];
+                    if ((c < '一' || c > '鿿') && !_extraChars.Contains(c))
+                    {
+                        sb.Clear();
+                        continue;
+                    }
+                    sb.Append(c);
+                    if (sb.Length == _windowSize)
+                    {
+                        string window = sb.ToString();
+                        sb.Remove(0, _wordLength);
+                        int maxFreq = 0;
+                        string maxWord = string.Empty;
+                        for (int j = 0; j < _wordLength; j++)
+                        {
+                            string _word = window.Substring(j, _wordLength);
+                            ref var _freq = ref CollectionsMarshal.GetValueRefOrNullRef(_freqDict, _word);
+                            if (_freq < _filter) continue;
+                            if (_freq > maxFreq)
+                            {
+                                maxFreq = _freq;
+                                maxWord = _word;
+                            }
+                        }
+                        if (maxFreq == 0) continue;
+                        ref var freq = ref CollectionsMarshal.GetValueRefOrNullRef(_resultDict, maxWord);
+                        if (Unsafe.IsNullRef(ref freq)) _resultDict.Add(maxWord, 1);
+                        else freq++;
+                    }
+                }
+            }
+        }
+
+        private IEnumerable<KeyValuePair<string, int>> Sort()
+        {
+            return _filter > 1
+                ? _resultDict.Where(x => x.Value >= _filter)
+                             .OrderByDescending(x => x.Value)
+                : _resultDict.OrderByDescending(x => x.Value);
+        }
+
+        private void Output(IEnumerable<KeyValuePair<string, int>> sortedDict)
+        {
+            if (!sortedDict.Any()) throw new InvalidOperationException("没有符合条件的词。");
+            using StreamWriter writer = new(_writePath, false, Encoding.UTF8);
+            writer.WriteLine("词\t频率");
+            foreach (var item in sortedDict)
+                writer.WriteLine($"{item.Key}\t{item.Value}");
+        }
+
+        public void Run()
+        {
+            try
+            {
+                LoadAllWords();
+                Console.WriteLine("第一轮统计结束。");
+                LoadGoodWords();
+                Console.WriteLine("第二轮统计结束。");
+                Output(Sort());
+                Console.WriteLine("统计结束，词频文件已生成。");
+            }
+            catch (Exception ex)
+                when (ex is InvalidOperationException
+                         or IOException
+                         or ArgumentException)
+            {
+                Console.WriteLine($"错误：{ex.Message}");
+            }
+        }
+    }
+}
---
+++ WordFreqCounter/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace WordFreqCounter
+{
+    internal class Program
+    {
+        private static int GetWordLength()
+        {
+            Console.Write("请指定要统计的词长：");
+            int wordLength;
+            while (!int.TryParse(Console.ReadLine(), out wordLength) || IsInvalidWordLength(wordLength))
+                Console.Write($"词长错误，请输入一个大于1且小于{int.MaxValue / 2 + 1}的整数！");
+            return wordLength;
+        }
+
+        private static bool IsInvalidWordLength(int wordLength) => wordLength is < 1 or > (int.MaxValue / 2 + 1);
+
+        private static string GetReadPath()
+        {
+            Console.Write("请输入要读取的文件路径，或将\"语料.txt\"放在当前目录下并按回车键：");
+            for (; ; )
+            {
+                string? readPath = Console.ReadLine();
+                if (string.IsNullOrEmpty(readPath)) readPath = Path.Combine(".", "语料.txt");
+                if (File.Exists(readPath)) return readPath;
+                Console.Write("找不到文件，请重试！");
+            }
+        }
+
+        private static int GetFilter()
+        {
+            Console.Write("请指定过滤频率，低于该频率的词将被忽略，留空则默认为2，可以为0：");
+            return int.TryParse(Console.ReadLine(), out int filter) ? filter : 2;
+        }
+
+        private static HashSet<int> GetExtraChars()
+        {
+            Console.Write("请输入要纳入的非中文字符，留空则只统计\\u4e00-\\u9fff：");
+            HashSet<int> extraChars = new(0);
+            foreach (char c in Console.ReadLine() ?? string.Empty)
+                extraChars.Add(c);
+            return extraChars;
+        }
+
+        private static void Main(string[] args)
+        {
+            if (args.Length == 4) RunWithArgs(args);
+            else RunWithoutArgs();
+        }
+
+        /// <summary>
+        /// 第一个参数为词长
+        /// 第二个参数为读取的文件路径
+        /// 第三个参数为过滤频率
+        /// 第四个参数为要纳入的非中文字符
+        /// </summary>
+        /// <param name="args"></param>
+        private static void RunWithArgs(string[] args)
+        {
+            if (int.TryParse(args[0], out int wordLength) && !IsInvalidWordLength(wordLength))
+            {
+                if (File.Exists(args[1]))
+                {
+                    string writePath = Path.Combine(Path.GetDirectoryName(args[1]) ?? ".", $"{wordLength}字统计结果.txt");
+                    if (int.TryParse(args[2], out int filter))
+                    {
+                        HashSet<int> extraChars = new(0);
+                        foreach (char c in args[3])
+                            extraChars.Add(c);
+                        Counter counter = new(args[1], writePath, extraChars, wordLength, filter);
+                        counter.Run();
+                    }
+                }
+            }
+            Console.Write("按任意键退出。");
+            Console.ReadKey();
+        }
+
+        private static void RunWithoutArgs()
+        {
+            for (; ; )
+            {
+                int wordLength = GetWordLength();
+                string readPath = GetReadPath();
+                string writePath = Path.Combine(Path.GetDirectoryName(readPath) ?? ".", $"{wordLength}字统计结果.txt");
+                int filter = GetFilter();
+                HashSet<int> extraChars = GetExtraChars();
+                Counter counter = new(readPath, writePath, extraChars, wordLength, filter);
+                counter.Run();
+                Console.Write("按任意键重新指定参数并进行新一轮统计。");
+                Console.ReadKey();
+            }
+        }
+    }
+}
